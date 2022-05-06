40fe6539d25757cbca75177cc80dfa3b63410ea4
ellsonmendesyt <elsonmendesoficial@gmail.com>
2022-05-06 12:51:56 -0300

    criei o servidor

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+
+const app = express();
+
+
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({status:'success',message:'Welcome to the API'});
+})
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})

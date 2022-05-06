ed4b02511e850c670b46b287d262cef5fa526211
ellsonmendesyt <elsonmendesoficial@gmail.com>
2022-05-06 15:58:50 -0300

    conectado ao mongo local

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const log=console.log;
+const env = require('dotenv').config();
+const conectar= require('./bd.js');
 
 
 const app = express();
+app.use(express.json());
 
 
 
+
+
+
+const customMiddleware = (req, res, next) => {
+    log('global middleware');
+    next();
+}
+
+// app.use(customMiddleware); // usar pra todas as rotas
 
 app.get('/',(req,res)=>{
+  console.log(req)
+    
+   
     res.status(200).json({status:'success',message:'Welcome to the API'});
+})
+
+
+
+app.get('/about',customMiddleware, (req,res,next)=>{
+    console.log('about page');
+    res.status(200).send("about");
 })
 
 
@@ -17,6 +41,21 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+
+
+
+
+
+
+// contar com bando de dados
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connection.on("connected",()=>{
+    console.log("Coenectado ao banco...")
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+
+mongoose.connection.on("error",(error)=>{
+    console.log("Erro ao conectar ao banco "+error)
 })
